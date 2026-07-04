fc65eec11a086858f6880d7cb0ce29a374066c59
Marina Marhitych <margmarina9@gmail.com>
2026-07-04 18:13:01 +0300

    feat: add Habit model

---
+++ HabitTracker/Models/HabitEntry.swift
@@ -0,0 +1,8 @@
+//
+//  HabitEntry.swift
+//  HabitTracker
+//
+//  Created by Marina Marhitych on 04.07.2026.
+//
+
+import Foundation
---
+++ HabitTracker/Models/HabitFrequency.swift
@@ -0,0 +1,8 @@
+//
+//  HabitFrequency.swift
+//  HabitTracker
+//
+//  Created by Marina Marhitych on 04.07.2026.
+//
+
+import Foundation
