9a2e1e2073940c28a5131750db954875c021c5cf
Marina Marhitych <margmarina9@gmail.com>
2026-07-04 18:26:45 +0300

    feat: wire up SwiftData ModelContainer in app entry point

--- HabitTracker/HabitTrackerApp.swift
+++ HabitTracker/HabitTrackerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct HabitTrackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Habit.self, HabitEntry.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
--- HabitTracker/Models/HabitFrequency.swift
+++ HabitTracker/Models/HabitFrequency.swift
@@ -10,4 +10,13 @@ import Foundation
 enum HabitFrequency: Codable, Hashable {
     case daily
     case timePerWeek(Int)
+    
+    var label: String {
+        switch self {
+        case .daily:
+            return "Every day"
+        case .timePerWeek(let cound):
+            return "\(cound) days per week"
+        }
+    }
 }
