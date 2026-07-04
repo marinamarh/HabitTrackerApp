f7b5d6515cf79715ff2464a7e727bdec02d5a6f4
Marina Marhitych <margmarina9@gmail.com>
2026-07-04 18:15:24 +0300

    feat: add HabitEntry model for daily completion records

--- HabitTracker/Models/HabitEntry.swift
+++ HabitTracker/Models/HabitEntry.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class HabitEntry {
+    @Attribute(.unique) var id: UUID
+    
+    var date: Date
+    var completionCount: Int
+    var habit: Habit?
+    
+    init(date: Date = .now, completionCount: Int = 1, habit: Habit? = nil) {
+        self.id = UUID()
+        self.date = Calendar.current.startOfDay(for: date)
+        self.completionCount = completionCount
+        self.habit = habit
+    }
+}
